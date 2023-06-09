e63eea99002fdd40cece8aabd07d4e85c4125345
MateoCraviotto <macraviotto@fi.uba.ar>
2023-06-09 14:13:39 -0300

    test: dado un saldo 50 quiero transferir 10 obtengo 40

--- spec/gestor_de_saldo_spec.rb
+++ spec/gestor_de_saldo_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 require './dominio/gestor_de_saldo'
 
-def crear_gestor_con_saldo(saldo)
-  gestor = described_class.new(nil)
+def crear_billetera_con_saldo(saldo)
+  gestor = GestorDeSaldo.new(nil)
   gestor.cargar_saldo(saldo)
   gestor
 end
@@ -19,19 +19,40 @@ describe GestorDeSaldo do
       expect(gestor.saldo).to eq 500
     end
     it 'dado un saldo 10 quiero cargar 500 obtengo 510' do
-      gestor = crear_gestor_con_saldo(10)
+      gestor = crear_billetera_con_saldo(10)
       gestor.cargar_saldo(500)
       expect(gestor.saldo).to eq 510
     end
     it 'dado un saldo 10 quiero cargar -10 obtengo 10' do
-      gestor = crear_gestor_con_saldo(10)
+      gestor = crear_billetera_con_saldo(10)
       gestor.cargar_saldo(-10)
       expect(gestor.saldo).to eq 10
     end
     it 'dado un saldo 10 quiero cargar 0 obtengo 10' do
-      gestor = crear_gestor_con_saldo(10)
+      gestor = crear_billetera_con_saldo(10)
       gestor.cargar_saldo(0)
       expect(gestor.saldo).to eq 10
     end
+  end
+
+  describe 'transferir' do
+    it 'dado un saldo 50 quiero transferir 10 obtengo 40' do
+      billetera = crear_billetera_con_saldo(50)
+      usuario = MockUsuario.new(0)
+      billetera.transferir(usuario, 10)
+      expect(billetera.saldo).to eq 40
+    end
+  end
+end
+
+class MockUsuario
+  attr_accessor :saldo
+
+  def initialize(saldo)
+    @saldo = crear_billetera_con_saldo(saldo)
+  end
+
+  def cargar_saldo(monto)
+    @saldo.cargar_saldo(monto)
   end
 end
