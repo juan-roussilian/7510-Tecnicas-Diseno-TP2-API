23cd8bc12aa9fb9adcaec769cfbb673075b3d8e6
Juan <jroussilian@fi.uba.ar>
2023-06-10 19:36:07 -0300

    refactor: Creo carpeta de mocks en spec y muevo el mock de repositorio usuarios

--- spec/grupo_spec.rb
+++ spec/grupo_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require './dominio/grupo'
+require './spec/mocks/mock_repositorio_usuarios'
 
 describe Grupo do
   describe 'new' do
@@ -19,29 +20,6 @@ describe Grupo do
       repo = MockRepositorioUsuarios.new
       repo.load_example_users(1)
       expect { Grupo.new('nombre', %w[user1], repo) }.to raise_error(MiembrosInsuficientesParaGrupo)
-    end
-  end
-end
-
-class MockRepositorioUsuarios
-  def initialize
-    @users = []
-  end
-
-  def find_by_telegram_username(telegram_username)
-    user = @users.find { |u| u.telegram_username == telegram_username }
-    raise ObjectNotFound.new(self.class.model_class, telegram_username) if user.nil?
-
-    user
-  end
-
-  def save(user)
-    @users << user
-  end
-
-  def load_example_users(number_of_users)
-    (0..number_of_users).each do |i|
-      save(Usuario.new("usuario#{i}", "test#{i}@test.com", i.to_s, "user#{i}"))
     end
   end
 end
---
+++ spec/mocks/mock_repositorio_usuarios.rb
@@ -0,0 +1,22 @@
+class MockRepositorioUsuarios
+  def initialize
+    @users = []
+  end
+
+  def find_by_telegram_username(telegram_username)
+    user = @users.find { |u| u.telegram_username == telegram_username }
+    raise ObjectNotFound.new(self.class.model_class, telegram_username) if user.nil?
+
+    user
+  end
+
+  def save(user)
+    @users << user
+  end
+
+  def load_example_users(number_of_users)
+    (0..number_of_users).each do |i|
+      save(Usuario.new("usuario#{i}", "test#{i}@test.com", i.to_s, "user#{i}"))
+    end
+  end
+end
